05fb8d5719ebe893c6505a9f0e8a0817e9da1c17
tatuamarrado66987 <107261106+tatuamarrado66987@users.noreply.github.com>
2024-03-20 11:17:30 -0300

    2024
    2024

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const botoes = document.querySelectorAll(".botao");
-for(let i=0;i >botoes.length;i++){
-    botoes[i].onclick =function() {
-        for (let j=0;i <botoes.length;j++) 
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick =function(){
+        for(let j=0;j <botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
         
+        botoes[i].classList.add("ativo");
     }
 }
