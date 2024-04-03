340b395710f1378ff5f9f1b4c98ce717b33af5f7
tatuamarrado66987 <107261106+tatuamarrado66987@users.noreply.github.com>
2024-04-03 10:35:03 -0300

    n
    n

--- main.js
+++ main.js
@@ -9,3 +9,23 @@ for(let i=0;i <botoes.length;i++){
         botoes[i].classList.add("ativo");
     }
 }
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo")
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+
+        for(let j=0;<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo")
+        }
+
+        botoes[i].classList.add("ativo")
+        textos[i].classList.add("ativo")
+    }
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new date("2024-10-05T00:00:00")
+let tempoAtual = new date();
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
